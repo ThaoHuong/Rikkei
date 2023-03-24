f83673cb7693c00db9007842b1e54523d55ebe6c
ThaoHuong <NguyenThaoHuongHY@gmail.com>
2023-03-24 23:14:22 +0900

    add code

---
+++ Buoi9/script/script.js
@@ -0,0 +1,18 @@
+alert('hello');
+// quy tắc camel case
+var tenBien;
+// quy tắc snake key
+var ten_bien;
+var ten,tuoi,diachi;
+
+// gán giá trị cho var
+var so = 12345, chuoi = '',chuoi = "", chuoi= ``;
+var check = true; //or false
+var $null=null, _under=undefined;
+
+// check các kiểu dữ liệu sử dụng typeof
+alert(typeof so); 
+alert(typeof $null);
+alert(typeof chuoi);
+alert(typeof check);
+alert(typeof _under);
