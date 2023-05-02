23ba00b9ff95a297372c279a8e3ec63c903c08bf
ThaoHuong <NguyenThaoHuongHY@gmail.com>
2023-05-02 20:33:20 +0900

    add code

---
+++ Buoi19/Origine/assets/js/script.js
@@ -0,0 +1,26 @@
+// $(document).ready(function () {
+//   $(".slick-track-bottom").owlCarousel({
+//     margin: 30,
+//     nav: true,
+//     prevArrow:
+//       '<button type="button" class="slick-prev"> <i class="icon-chevron-left"> </i></button>',
+//     nextArrow:
+//       '<button type="button" class="slick-next"><i class="icon-chevron-right"> </i></button>',
+//     autoplay: true,
+//     loop: true,
+//     dots: true,
+//     responsive: {
+//       // breakpoint from 480 up
+//       0: {
+//         items: 2,
+//       },
+//       768: {
+//         items: 3,
+//       },
+//       // breakpoint from 768 up
+//       992: {
+//         items: 6,
+//       },
+//     },
+//   });
+// });
